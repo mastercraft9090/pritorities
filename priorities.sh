036e580aaa6d2be6535c409e9bb263bc51156c1b
mastercraft9090 <mastercraft9090@gmail.com>
2023-05-02 17:02:06 +0300

    my first commit

---
+++ priorities.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+
+now="$(date)" 
+echo "Today is **** $now **** Let us get ready to be Productive!!!" \n\n
+
+echo "What is the first task to complete today?"
+
+read task1
+
+echo "Your first task is $task1"
+
+
+
